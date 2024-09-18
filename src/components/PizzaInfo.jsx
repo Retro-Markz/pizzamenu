8147d6ded1a1783014a2c27229d0aac679cecfcc
Retro-Markz <cubedomarco@gmail.com>
2024-09-18 03:30:44 -0700

    ok done v2

--- src/components/PizzaInfo.jsx
+++ src/components/PizzaInfo.jsx
@@ -3,7 +3,7 @@ import "../styles/pizzaInfo.css";
 
 const PizzaInfo = ({ pizzaObj }) => {
   return (
-    <li className="list-style ">
+    <li className={`list-style  ${pizzaObj.soldOut ? "sold-Out" : ""}`}>
       <img
         className="w-32 rounded-full shadow-md"
         src={pizzaObj.photoName}
@@ -14,7 +14,10 @@ const PizzaInfo = ({ pizzaObj }) => {
       </h1>
       <p className="w-56 text-center">{pizzaObj.ingredients}</p>
       <h2 className="mt-2 font-semibold text-xl ">
-        price $<span className="text-red-800">{pizzaObj.price}</span>
+        price $
+        <span className="text-red-800">
+          {pizzaObj.soldOut ? "Sold Out" : pizzaObj.price}
+        </span>
       </h2>
     </li>
   );
